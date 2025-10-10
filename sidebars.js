29d0b43c6683225b4317144bf1bcd8a11911e1a9
Mohit <mohityadav@tetheria.ai>
2025-10-10 16:23:37 -0700

    Conflict fix

--- sidebars.js
+++ sidebars.js
@@ -27,18 +27,6 @@ const sidebars = {
     // 'teleop',                   // Teleoperation
     // 'hand_sim',                 // Hand simulation
   ],
-    'intro',                    // Introduction page
-    'assembly',                 // Assembly Instructions
-    'mechanical',               // Mechanical documentation
-    'getting_started',          // Getting Started guide
-    'firmware',                 // Firmware documentation
-    'sdk',                      // SDK documentation
-    'hand_sim',                 // Hand simulation
-    'pcb',                      // PCB documentation
-    'ros2_comm',                // ROS2 communication
-    'teleop',                   // Teleoperation
-    
-  ],
 };
 
 export default sidebars;
